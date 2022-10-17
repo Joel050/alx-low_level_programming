d151d1c9edb507e3311bbc67260360b38b5d9062
Joel <joel@joomhq.com>
2022-10-17 13:44:21 -0700

    task 002

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- writing program that prints alphabet in lower case
+ *Return:0 
+ */
+int main(void)
+{
+	char i;
+i
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
