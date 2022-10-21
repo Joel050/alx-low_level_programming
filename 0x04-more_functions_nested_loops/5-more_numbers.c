99e6605acba2b631491189d52ce96da5e756ab59
Joel <joel@joomhq.com>
2022-10-20 17:10:35 -0700

    debugging 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - print more numbers within 0 and 14
+ * 10 times
+ * Return: no return
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar('1');
-			-putchar (j % 10 + '0');
+				_putchar((j / 10) + 48);
+			-putchar ((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
