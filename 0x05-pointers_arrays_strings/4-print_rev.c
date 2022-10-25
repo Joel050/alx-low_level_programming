d2de5d17d10d08d6f98e95679fb447e90184a962
Joel <joel@joomhq.com>
2022-10-25 14:16:29 -0700

    task 4 correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,14 @@
 void print_rev(char *s)
 {
 	int count = 0;
+
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
 			break;
 		count++;
-											}
+	}
+
 	for (count--; count >= 0; count--)
 		_putchar(s[count]);
 	_putchar('\n');
